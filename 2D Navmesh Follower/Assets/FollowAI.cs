34e1a938dd4310105eb2df1a20fc86480e4ef1a6
harisalibaig <h.of.harris@gmail.com>
2019-09-12 21:52:35 +0500

    2d Navmesh

---
+++ 2D Navmesh Follower/Assets/FollowAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowAI : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent agent;
+    public GameObject target;     
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.transform.position);
+    }
+}
---
+++ 2D Navmesh Follower/Assets/WaitingRoom/Scripts/JesseController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JesseController : MonoBehaviour
+{
+    public Transform target;
+    public float offSet;
+	private Animator animController;
+	public UnityEngine.AI.NavMeshAgent myAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+		animController = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (myAgent.velocity.x == 0)
+		{
+			animController.SetInteger("Direction", 0);
+		}
+		else if(myAgent.velocity.z>0.5f)
+		{
+			animController.SetInteger("Direction",1);
+		}
+		else if (myAgent.velocity.z < -0.5f)
+		{
+			animController.SetInteger("Direction", 2);
+		}
+		else if (myAgent.velocity.x > 0.5f)
+		{
+			animController.SetInteger("Direction", 3);
+		}
+		else if (myAgent.velocity.x < -0.5f)
+		{
+			animController.SetInteger("Direction", 4);
+		}
+	}
+    void LateUpdate()
+    {
+        transform.localPosition = new Vector3(target.localPosition.x,transform.localPosition.y,target.localPosition.z+offSet);
+    }
+}
